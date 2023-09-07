b7114f08984abead8b09397127a7fbf923ef81ca
shubhampathak <shubham.pathak926@gmail.com>
2023-09-07 18:34:00 +0530

    client build updatation

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
-import path from 'path'
+import path from "path";
 import { fileURLToPath } from "url";
 dotenv.config();
 connectDB();
 
-// esmodule fix 
+// esmodule fix
 const __Filename = fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__Filename)
+const __dirname = path.dirname(__Filename);
+
 const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
-
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 
@@ -29,12 +29,10 @@ app.use(express.static(path.join(__dirname,'./client/build')))
 //   });
 // });
 
-
 app.use("/api/v1/auth", authRoutes);
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-    })
-
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(
